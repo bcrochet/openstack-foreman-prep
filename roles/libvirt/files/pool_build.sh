e37ef2360bf8683f6537e8ed940613d75cded55f
Brad P. Crochet <brad@redhat.com>
2014-03-19 07:50:40 -0400

    Massive update. More friendly to different OS per guest

--- roles/libvirt/files/pool_build.sh
+++ roles/libvirt/files/pool_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 poolpath=${1}
 virsh pool-destroy default
+virsh pool-undefine default
 virsh pool-define-as --name default --type dir --target $poolpath
 virsh pool-autostart default
 virsh pool-build default
